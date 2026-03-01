4d637c7e378a4fe17691b4dde99097207e06a5ca
VictorAgahi <victor@agahi.com>
2026-03-01 22:48:43 +0100

    grpc connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/app-config.module.js';
 import { DatabaseModule } from './providers/database/database.module.js';
 import { EventModule } from './modules/event/event.module.js';
+import { GrpcClientModule } from './grpc/grpc-client.module.js';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule, EventModule],
+  imports: [AppConfigModule, DatabaseModule, EventModule, GrpcClientModule],
 })
 export class AppModule {}
--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EventController } from './event.controller.js';
+import { EventGrpcController } from './event.grpc.controller.js';
 
 @Module({
-  controllers: [EventController],
+  controllers: [EventController, EventGrpcController],
 })
 export class EventModule {}
