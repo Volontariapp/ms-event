ea9f351595d99246a6ed59387cb65998e7cd5045
VictorAgahi <victor@agahi.com>
2026-03-01 16:18:54 +0100

    feat: scaffold project architecture with wired modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppConfigModule } from './config/app-config.module';
+import { DatabaseModule } from './providers/database/database.module';
+import { EventModule } from './modules/event/event.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [AppConfigModule, DatabaseModule, EventModule],
 })
 export class AppModule {}
