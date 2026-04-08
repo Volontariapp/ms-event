6f2c9e51e9e07cf278775820192aaf47e8d48026
VictorAgahi <victor@agahi.com>
2026-04-08 14:49:42 +0200

    feat: setup custom structured logger

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { NestFactory } from '@nestjs/core';
 import { GRPC_SERVICES, getGrpcOptions } from '@volontariapp/contracts';
 import { AppModule } from './app.module.js';
 import { AppConfigService } from './config/app-config.service.js';
+import { Logger } from '@volontariapp/logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger({ context: 'MS-EVENT', format: 'json' });
+  const app = await NestFactory.create(AppModule, {
+    logger,
+  });
+  app.useLogger(logger);
   const configService = app.get(AppConfigService);
 
   app.connectMicroservice(
