69f8a91f2f25f3f292963ec8b58eaf6e274df004
VictorAgahi <victor@agahi.com>
2026-05-01 13:00:26 +0200

    feat(husky): pre-commit

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -33,7 +33,13 @@ export const AppDataSource = new DataSource({
   password: appConfig.db.password,
   database: appConfig.db.database,
   ssl: appConfig.db.ssl ? { rejectUnauthorized: false } : false,
-  entities: [EventModel, TagModel, RequirementModel, EventQueueModel, JobsOutboxModel],
+  entities: [
+    EventModel,
+    TagModel,
+    RequirementModel,
+    EventQueueModel,
+    JobsOutboxModel,
+  ],
   migrations: [
     join(dirname(fileURLToPath(import.meta.url)), '..', 'migrations', '*.ts'),
   ],
